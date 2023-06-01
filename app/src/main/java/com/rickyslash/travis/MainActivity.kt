3f667cc6140a01568ba4ca3da49b18c752c87fa3
ricky-kiva <ricky.kiva@gmail.com>
2023-06-01 23:55:09 +0700

    EditText on Main Activity

--- app/src/main/java/com/rickyslash/travis/MainActivity.kt
+++ app/src/main/java/com/rickyslash/travis/MainActivity.kt
@@ -2,10 +2,15 @@ package com.rickyslash.travis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rickyslash.travis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy(LazyThreadSafetyMode.NONE) { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
