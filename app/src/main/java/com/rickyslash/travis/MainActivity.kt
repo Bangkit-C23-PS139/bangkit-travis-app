b0e2b9d03cbd442fe0ea5813742947e8dcceaaf5
ricky-kiva <ricky.kiva@gmail.com>
2023-06-04 13:14:31 +0700

    add BottomNavigation

--- app/src/main/java/com/rickyslash/travis/MainActivity.kt
+++ app/src/main/java/com/rickyslash/travis/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rickyslash.travis
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -22,14 +23,18 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_meet,
+                R.id.navigation_bookmark,
+                R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.apply {
+            setBackgroundColor(ContextCompat.getColor(applicationContext, android.R.color.transparent))
+            setupWithNavController(navController)
+        }
     }
 }
